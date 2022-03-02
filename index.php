7343aa819a9daf3b84ffaa2ab172b989f08a2693
Matthew Hays <100310623+KingPotato117@users.noreply.github.com>
2022-03-02 15:09:01 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,91 @@
 <?php
     require("model.php");
+         
+    session_set_cookie_params(4000, '/');          
+    session_start();
+
+    $dsn = 'mysql:host=localhost;dbname=PeerEval';
+    $dbusername = 'prototype';
+    $dbpassword = 'CS495';
+
     include("navBar.html");
 
-    if (isset($_GET["createClass"])) {  //display createClassroom page 
-        include("createClass.html");
-    } else if(isset($_GET["joinClass"])) {
-        include("joinClass.html");
-    } else {                            //display main page 
-        include("home.html");
+    try {
+        $db = new PDO($dsn, $dbusername, $dbpassword);
+        //handle GET requests
+        if (isset($_GET["createClass"])) {  //display createClassroom page 
+            include("createClass.html");
+        } else if(isset($_GET["joinClass"])) { //display joinClassroom page
+            include("joinClass.html");
+        } else if (isset($_GET["read"])) {  //display techer read page 
+            include("read.html");
+        } else if (isset($_GET['student'])) {
+            include("joinClassStudent.html");
+            $students = getStudents($db);
+            echo "<form method='POST'>";
+            echo "<label for='students' >Please select your name: </label>";
+            echo "<select name='students' >";
+
+            foreach ($students as $student) {
+                echo "<option value='". $student["studentName"] . "' >". $student["studentName"] .  "</option>";
+            }
+
+            echo "<input type='submit' name='studentLogin' />";
+            echo "</select>";
+            echo "</form>";
+        }else if (isset($_GET['teacher'])) {
+            include("joinClassTeacher.html");
+        } else if ($_GET['teacherView']) {
+            fetchTeacherData($db);
+            include("teacherView.html");
+        } else {                            //display main page 
+            include("home.html");
+        }
+
+        //handle POST requests 
+        if(isset($_POST['submitFile'])) {
+            if ($_FILES['studentGroups']['error'] == UPLOAD_ERR_OK        
+                  && is_uploaded_file($_FILES['studentGroups']['tmp_name'])) {
+                $contents = file_get_contents($_FILES['studentGroups']['tmp_name']);
+                $groups = explode(".", $contents);
+                foreach ($groups as &$group) {
+                    if ($group == "") {
+                        $blank = array_search("", $groups);
+                        unset($groups[$blank]);
+                    } else {
+                        $group = explode(", ", $group);
+                    }
+                }
+            createClassroom($db, $groups);
+            header("Refresh: 0; url=index.php?read");
+            }
+        } else if (isset($_POST['userType'])) {
+            $classId = joinClassRoom($db, $_POST['accessCode']);
+            if ($classId != false) {
+                $classId = $classId['classId'];
+                $_SESSION['classId'] = $classId;
+                $_SESSION['accessCode'] = $_POST['accessCode'];
+                if ($_POST['userType'] == 'Student') {
+                    header("Refresh: 0; url=index.php?student");
+                } else if ($_POST['userType'] == 'Teacher') {
+                    header("Refresh: 0; url=index.php?teacher");
+                } 
+            } else {
+                echo "<p>Class with code " . $_POST['accessCode'] . " does not exist! Check code and try again.</p>";
+            }
+        } else if (isset($_POST['teacherLogin'])) {
+            $givenPass = $_POST['password'];
+            $result = checkPassword($db, $_SESSION['accessCode']);
+            $pass = $result['classPassword'];
+            if ($givenPass == $pass) {
+                $_SESSION['teacherLoggedIn'] = true;
+                header("Refresh: 0; url=index.php?teacherView");
+            } else {
+                echo "<p>Password given does not match password on record, please try again. </p>";
+            }
+        }
+    } catch (PDOException $e) {
+
     }
 
     include("footer.html");
--- model.php
+++ model.php
@@ -1,9 +1,97 @@
 <?php
-    function createClassRoom($db) {
-        
+    function getRandPass(): String {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $pieces = [];
+        $max = mb_strlen($characters, '8bit') - 1;
+        for ($i = 0; $i < 16; ++$i) {
+            $pieces []= $characters[random_int(0, $max)];
     }
-    function joinClassRoom($db) {
+    return implode('', $pieces);
+    }
 
+    function createClassRoom($db, $groups) { //still needs error checking
+        try {
+        $accessCode = random_int(0, 10000);
+        $classPass = getRandPass();
+    
+        //create new classRoom table entry    
+        $createClass = "INSERT INTO `Classrooms` (accessCode, classPassword) VALUES (:accessCode, :classPass)";
+        $statement = $db->prepare($createClass);
+        $statement->bindValue(':accessCode', $accessCode);
+        $statement->bindValue(':classPass', $classPass);
+        $success = $statement->execute();
+        var_dump($success);
+
+        $getClassId = "SELECT classId FROM `Classrooms` WHERE accessCode = :accessCode AND classPassword = :classPass";
+        $statement = $db->prepare($getClassId);
+        $statement->bindValue(':accessCode', $accessCode);
+        $statement->bindValue(':classPass', $classPass);
+        $statement->execute();
+        $classId = $statement->fetch();
+        $cId = $classId['classId'];
+
+        $insertStudent = "INSERT INTO `Students` (studentName, classId) VALUES (:studentName, :classId)";
+
+        //parse groups 
+        foreach ($groups as $group){
+                //go through each name in group, format string properly for group and insert into students table  
+                $names = "";
+                foreach ($group as $name) {
+                    $name = ltrim($name, "\n ");
+                    if ($name == end($group)) {
+                        $names .= $name;
+                    } else {
+                        $names .= $name . ", ";
+                    }
+                    $statement = $db->prepare($insertStudent);
+                    $statement->bindValue(':studentName', $name);
+                    $statement->bindValue(':classId', $cId);
+                    $statement->execute();
+                }
+                //trim whitespace and newlines
+                $names = ltrim($names, "\n ");
+
+                $insertGroups = "INSERT INTO `groups` (names, classId) VALUES (:names, :classId)";
+                $statement = $db->prepare($insertGroups);
+                $statement->bindvalue(':names', $names);
+                $statement->bindvalue(':classId', $cId);
+                $statement->execute();
+            }
+        } catch(PDOExecption $e) {}
+    }
+
+    function joinClassRoom($db, $accessCode) {
+        $checkClassId = "SELECT classId FROM `Classrooms` WHERE accessCode = :accessCode";
+        $statement = $db->prepare($checkClassId);
+        $statement->bindValue(':accessCode', $accessCode);
+        $statement->execute();
+        $res = $statement->fetch();
+        return $res;
+    }
+
+    function getStudents($db) {
+        $getNames = "SELECT studentName FROM `Students` WHERE classId = :classId";
+        $statement = $db->prepare($getNames);
+        $statement->bindvalue(':classId', $_SESSION['classId']);
+        $statement->execute();
+        $res = $statement->fetchAll();
+        return $res;
+    }
+
+    function checkPassword($db, $accessCode) {
+        $checkPass = "SELECT classPassword FROM `Classrooms` WHERE accessCode = :accessCode";
+        $statement = $db->prepare($checkPass);
+        $statement->bindvalue(':accessCode', $accessCode);
+        $statement->execute();
+        $res = $statement->fetch();
+        return $res;
+    }
+
+    function fetchTeacherData($db) {
+        $classId = $_SESSION['classId'];
+
+        fetchGroupData = "SELECT groupId, names WHERE "
+        fetchGroupScoreData = "SELECT Score1, Score2, Score3, AvgScore, Comments, StudentId WHERE class"
     }
 
 
