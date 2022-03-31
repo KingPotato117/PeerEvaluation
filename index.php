9eafdadb14e85a4c81b43b1f3ab3a6c2a6214362
AndrewWalsh315 <82915664+AndrewWalsh315@users.noreply.github.com>
2022-03-31 17:57:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,18 @@
     $dbusername = 'prototype';
     $dbpassword = 'CS495';
     include("navBar.html");
-
+     //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+// Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    
+    
     try {
         $db = new PDO($dsn, $dbusername, $dbpassword);
         //handle GET requests
